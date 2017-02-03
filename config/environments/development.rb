7c39238952d61746ccafe335e98dd69772a282a0
josueaceves <josueaceves.ja@gmail.com>
2017-02-03 13:11:35 -0800

    update mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,15 +38,20 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "aceves-seo.com",
+    domain: "gmail.com",
     authentications: "plain",
-    enable_starttls_true: true,
+    enable_starttls_auto: true,
     username: ENV["GMAIL_ACCOUNT"],
-    passwords: ENV["GMAIL_PASSWORD"]
+    passwords: ENV["GMAIL_PASSWORD"],
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'none'
   }
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
